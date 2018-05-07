4881b214d98006999e1b915dadf3b3c5161e7c99
FoxAndre96 <s223523@studenti.polito.it>
2018-05-07 14:41:04 +0200

    Lab5

--- api.py
+++ api.py
@@ -12,23 +12,26 @@ def hello_world():
 
 @app.route('/listtasks')
 def ListTasks():
-    # get list of object into a python variable (here already created)
     tasks = dbfunctions.show_tasks()
+    for task in tasks:
+        print(task[0])
+    # need to convert tasks into a dictionary to 'help' jsonify
     return jsonify(tasks)
 
 
 @app.route('/listtasks/<new_task>')
 def GetTask(new_task):
-    task = [task for task in dbfunctions.show_tasks() if task[1] == new_task]
+    task = [task for task in dbfunctions.show_tasks() if task[0] == new_task]
     if len(task) == 1:
         return jsonify(task[1])
     else:
-        response = jsonify({'message': 'user not found: ' + name})
+        dbfunctions.add_new_task(new_task)
+        response = jsonify({'message': 'new task added: ' + new_task})
         response.status_code = 404
         return response
 
 
-@app.route('/listtasks', methods=['POST'])
+@app.route('/listtasks', methods=['DELETE'])
 def CreateTask():
     if request.headers['Content-Type'] == 'application/json':
         new_task = request.json
@@ -40,6 +43,20 @@ def CreateTask():
         return response
 
 
+@app.route('/listtasks/<task>', methods=['PUT'])
+def UpdateTask(new_task):
+    dbfunctions.remove_task(new_task)
+    if request.headers['Content-Type'] == 'application/json':
+        new_task = request.json
+        dbfunctions.add_new_task(new_task)
+        return jsonify(new_task)
+    else:
+        response = jsonify({'message': "Invalid Request"})
+        response.status_code = 404
+        return response
+
+
+
 if __name__ == '__main__':
 
-    app.run(port=5004)
+    app.run()
